e02bd7fdc6566e0588edf2cd3ad5f4e125129b14
max-borisov <max.borisov@yahoo.com>
2015-07-03 11:36:19 +0300

    Delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash[:alert] = "Your new post couldn't be created!  Please check the form."
+      render :new
+    end
   end
 
   def show
@@ -21,12 +26,18 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Update failed.  Please check the form."
+      render :edit
+    end
   end
 
   def destroy
     @post.destroy
+    flash[:success] = "Your post has been deleted."
     redirect_to posts_path
   end
 
