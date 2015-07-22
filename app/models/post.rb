3b31868618d969fb4e64fe0ec05558236d905e74
max-borisov <max.borisov@yahoo.com>
2015-07-22 12:08:29 +0300

    Up post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,11 @@ class Post < ActiveRecord::Base
   validates :user_id, presence: true
   validates :caption, length: { minimum: 2, maximum: 300 }
 
-  has_attached_file :image, styles: { medium: "640x" }
+  has_attached_file :image,
+    styles: { medium: "640x" },
+    path: ":rails_root/public/:class/:attachment/:id/:style_:basename.:extension",
+    url: "/:class/:attachment/:id/:style_:basename.:extension"
+
   validates_attachment :image, :presence => true,
     content_type: { content_type: /\Aimage\/.*\Z/ },
     size: { less_than: 2.megabytes }
